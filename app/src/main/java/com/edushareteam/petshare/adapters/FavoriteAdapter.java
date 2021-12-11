5404a62bde217f679fbcc7a838a78068cac498ad
Fatih Baser <basese_aehit@students.vizja.pl>
2021-12-11 21:20:02 +0100

    Favorite fixed problem

--- app/src/main/java/com/edushareteam/petshare/adapters/FavoriteAdapter.java
+++ app/src/main/java/com/edushareteam/petshare/adapters/FavoriteAdapter.java
@@ -52,9 +52,45 @@ public class FavoriteAdapter extends FirestoreRecyclerAdapter<Like, FavoriteAdap
             mTextViewNumberOfFavoriteItem.setText(String.valueOf(numberFilter));
         }
 
+        switch (like.getCategory()) {
+            case "Cats":
+                holder.imageViewPet.setImageResource(R.drawable.cat);
+                break;
+            case "Dogs":
+                holder.imageViewPet.setImageResource(R.drawable.dog);
+                break;
+            case "Birds":
+                holder.imageViewPet.setImageResource(R.drawable.bird);
+                break;
+            case "Rabbits":
+                holder.imageViewPet.setImageResource(R.drawable.rabbit);
+                break;
+            case "Horses":
+                holder.imageViewPet.setImageResource(R.drawable.horse);
+                break;
+            case "Ferrets":
+                holder.imageViewPet.setImageResource(R.drawable.ferret);
+                break;
+            case "Fish":
+                holder.imageViewPet.setImageResource(R.drawable.fish);
+                break;
+            case "Guinea Pigs":
+                holder.imageViewPet.setImageResource(R.drawable.pig);
+                break;
+            case "Rats and Mice":
+                holder.imageViewPet.setImageResource(R.drawable.rat);
+                break;
+            case "Amphibians":
+                holder.imageViewPet.setImageResource(R.drawable.turtle);
+                break;
+            case "Reptiles":
+                holder.imageViewPet.setImageResource(R.drawable.chameleon);
+                break;
+        }
+
         holder.delete.setOnClickListener(view -> deletePost(like.getId()));
         holder.textViewTitle.setText(like.getTitle());
-        holder.textViewCategory.setText(like.getCategory());
+        //holder.textViewCategory.setText(like.getCategory());
 
         if (like.getImage() != null) {
             if (!like.getImage().isEmpty()) {
@@ -98,6 +134,7 @@ public class FavoriteAdapter extends FirestoreRecyclerAdapter<Like, FavoriteAdap
 
         ImageView imageViewPost;
 
+        ImageView imageViewPet;
         ImageView delete;
         View viewHolder;
         ProgressBar bar;
@@ -109,6 +146,7 @@ public class FavoriteAdapter extends FirestoreRecyclerAdapter<Like, FavoriteAdap
             delete = view.findViewById(R.id.imageViewLike);
             imageViewPost = view.findViewById(R.id.imageViewPostCard);
 
+            imageViewPet=view.findViewById(R.id.imageViewPets);
             bar = view.findViewById(R.id.postLoading);
             viewHolder = view;
         }
